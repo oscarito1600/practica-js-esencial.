9544d3ac8561facfc0e016bbebde87b7bc76f407
Oscar Ramirez <barleyunmain@gmail.com>
2026-07-19 19:44:45 -0600

    Se realizo el ejercicio 4

--- scripts/ejercicio3.js
+++ scripts/ejercicio3.js
@@ -1,6 +1,6 @@
 let puntaje = 100;
 
 puntaje += 50;
-
+ 
 console.log("El puntaje final incrementado es: " + puntaje);
 alert("El puntaje final incrementado es: " + puntaje);
---
+++ scripts/ejercicio4.js
@@ -0,0 +1,9 @@
+let saludo = "Hola ";
+let usuario = "Carlos";
+
+let mensaje = saludo + usuario;
+
+
+console.log(mensaje);
+
+alert(mensaje);
