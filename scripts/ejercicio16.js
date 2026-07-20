7f41d5c39de5cebc3693f4c79bd9cdb160860e88
Oscar Ramirez <barleyunmain@gmail.com>
2026-07-20 10:52:18 -0600

    Ejercicio 16: Implementa un bucle do...while con una condición que sea falsa desde el inicio para demostrar que el bloque de código se ejecuta al menos una vez.

---
+++ scripts/ejercicio16.js
@@ -0,0 +1,8 @@
+let condicion = false;
+
+do {
+   
+    console.log("El bucle do...while se ejecutó aunque la condición es falsa.");
+    alert("El bucle do...while se ejecutó aunque la condición es falsa.");
+    
+} while (condicion === true); 
