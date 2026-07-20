30e30b3840db954666a31b8b098d63977b43f985
Oscar Ramirez <barleyunmain@gmail.com>
2026-07-19 21:05:30 -0600

    Se realizo el ejercicio 8

---
+++ scripts/ejercicio8.js
@@ -0,0 +1,6 @@
+let nota1 = 8;
+let nota2 = 6;
+let alMenosUnoAprobado = (nota1 >= 7 || nota2 >= 7);
+
+console.log("¿Al menos una nota es aprobatoria?: " + alMenosUnoAprobado);
+alert("¿Al menos una nota es aprobatoria?: " + alMenosUnoAprobado);
