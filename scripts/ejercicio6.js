efd41e0a0672bf477dc062e7ed1db0ab2425c1f1
Oscar Ramirez <barleyunmain@gmail.com>
2026-07-19 20:43:52 -0600

    Se realizo el ejercicio 6

---
+++ scripts/ejercicio6.js
@@ -0,0 +1,9 @@
+let igualdadDebil = (10 == "10");
+
+let igualdadEstricta = (10 === "10");
+
+console.log("Resultado con igualdad débil (==): " + igualdadDebil);
+console.log("Resultado con igualdad estricta (===): " + igualdadEstricta);
+
+alert("Igualdad débil (10 == '10'): " + igualdadDebil);
+alert("Igualdad estricta (10 === '10'): " + igualdadEstricta);
