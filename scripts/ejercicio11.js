b6cf24a2dd78ca76761a68a2932f1bb7411a09e8
Oscar Ramirez <barleyunmain@gmail.com>
2026-07-20 09:47:58 -0600

    Se realizo el ejercicio 11

---
+++ scripts/ejercicio11.js
@@ -0,0 +1,19 @@
+let color = "verde";
+
+switch (color) {
+    case "rojo":
+        console.log("Detenerse");
+        alert("Detenerse");
+        break;
+        
+    case "verde":
+        console.log("Avanzar");
+        alert("Avanzar");
+        break;
+        
+    default:
+        
+        console.log("Precaución");
+        alert("Precaución");
+        break;
+}
