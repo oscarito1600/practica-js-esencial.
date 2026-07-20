b0f4356c204b780e4b9f8991982c9dca406823c0
Oscar Ramirez <barleyunmain@gmail.com>
2026-07-19 21:19:39 -0600

    Se realizo el ejercicio 9

---
+++ scripts/ejercicio9.js
@@ -0,0 +1,10 @@
+let temperatura = 32;
+
+if (temperatura > 30) {
+    console.log("Hace calor");
+    alert("Hace calor");
+} else {
+    
+    console.log("Clima agradable");
+    alert("Clima agradable");
+}
