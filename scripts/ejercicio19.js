3223f05f581dd1f6d473c2a7477473709e62bd68
Oscar Ramirez <barleyunmain@gmail.com>
2026-07-20 11:21:59 -0600

    Ejercicio 19 terminado con su html y su js

---
+++ scripts/ejercicio19.js
@@ -0,0 +1,6 @@
+const multiplicarPorDos = numero => numero * 2;
+
+let resultado = multiplicarPorDos(5);
+
+console.log("El doble del número es: " + resultado);
+alert("El doble del número es: " + resultado);
