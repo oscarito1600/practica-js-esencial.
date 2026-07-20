7ab573672336fdf4f58cfc2c7f0730e2867a3936
Oscar Ramirez <barleyunmain@gmail.com>
2026-07-19 20:58:16 -0600

    Se realizo el ejercicio 7

---
+++ scripts/ejercicio7.js
@@ -0,0 +1,6 @@
+let nota1 = 8;
+let nota2 = 6;
+let ambosAprobados = (nota1 >= 7 && nota2 >= 7);
+
+console.log("¿Ambas notas son mayores o iguales a 7?: " + ambosAprobados);
+alert("¿Ambas notas son mayores o iguales a 7?: " + ambosAprobados);
