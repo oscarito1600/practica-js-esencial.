9cf1d7ce8080bc3d6706093f14cefe1bd4f5ada3
Oscar Ramirez <barleyunmain@gmail.com>
2026-07-19 20:20:24 -0600

    se realizo el ejercicio 5

---
+++ scripts/ejercicio5.js
@@ -0,0 +1,7 @@
+const frutas = ["Manzana", "Plátano"];
+
+frutas.push("Naranja");
+
+console.log("La longitud total del arreglo es: " + frutas.length);
+
+alert("La longitud total del arreglo es: " + frutas.length);
